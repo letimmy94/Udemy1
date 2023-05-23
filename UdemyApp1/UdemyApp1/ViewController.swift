e0d2baf9666ed710c56f74e90c98b10cd1d6f1c0
Timmy Le <le.timmy94@gmail.com>
2023-05-23 18:59:46 -0400

    first commit

---
+++ UdemyApp1/UdemyApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UdemyApp1
+//
+//  Created by Timmy Le on 5/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
