f00f44aa564d6e9a041f991e16cb5b0519e53d7c
Timmy Le <le.timmy94@gmail.com>
2023-05-29 23:25:29 -0400

    Added button

--- UdemyApp1/UdemyApp1/ViewController.swift
+++ UdemyApp1/UdemyApp1/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var greetings1: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
