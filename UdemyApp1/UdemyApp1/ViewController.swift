43f8e910154a919340c162e3d87909b0f6bada0a
Timmy Le <le.timmy94@gmail.com>
2023-05-31 08:04:06 -0400

    Button changes languages

--- UdemyApp1/UdemyApp1/ViewController.swift
+++ UdemyApp1/UdemyApp1/ViewController.swift
@@ -8,13 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var greetings1: UIButton!
+    
+    @IBOutlet weak var startImage: UIImageView!
+    
+    var greetingImage = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    
+        
     }
 
 
+    @IBAction func switchLanguages(_ sender: UIButton) {
+        startImage.image = [#imageLiteral(resourceName: "12"), #imageLiteral(resourceName: "7" ), #imageLiteral(resourceName: "9"), #imageLiteral(resourceName: "8"), #imageLiteral(resourceName: "13"), #imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "1"), #imageLiteral(resourceName: "14"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "3"), #imageLiteral(resourceName: "7"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "16"), #imageLiteral(resourceName: "11"), #imageLiteral(resourceName: "17"), #imageLiteral(resourceName: "8"), #imageLiteral(resourceName: "5")][greetingImage]
+        
+        greetingImage = greetingImage + 1
+    }
 }
 
