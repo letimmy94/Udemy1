c79d3b33f3dab42c485cb8762d00dcb0e4a81b9b
Timmy Le <le.timmy94@gmail.com>
2023-06-05 08:46:26 -0400

    added language

--- UdemyApp1/UdemyApp1/ViewController.swift
+++ UdemyApp1/UdemyApp1/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
 
 
     @IBAction func switchLanguages(_ sender: UIButton) {
-        startImage.image = [#imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "10"), #imageLiteral(resourceName: "1"), #imageLiteral(resourceName: "12"), #imageLiteral(resourceName: "11"), #imageLiteral(resourceName: "13"), #imageLiteral(resourceName: "10"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "6"), #imageLiteral(resourceName: "15"), #imageLiteral(resourceName: "14"), #imageLiteral(resourceName: "16"), #imageLiteral(resourceName: "8"), #imageLiteral(resourceName: "7"), #imageLiteral(resourceName: "17"), #imageLiteral(resourceName: "8")][Int.random(in: 0...16)]
+        startImage.image = [#imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "10"), #imageLiteral(resourceName: "1"), #imageLiteral(resourceName: "12"), #imageLiteral(resourceName: "11"), #imageLiteral(resourceName: "13"), #imageLiteral(resourceName: "10"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "6"), #imageLiteral(resourceName: "15"), #imageLiteral(resourceName: "14"), #imageLiteral(resourceName: "16"), #imageLiteral(resourceName: "8"), #imageLiteral(resourceName: "7"), #imageLiteral(resourceName: "17"), #imageLiteral(resourceName: "8"), #imageLiteral(resourceName: "18")][Int.random(in: 0...17)]
         
         greetingImage += 1
     }
