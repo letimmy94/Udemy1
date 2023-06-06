e38d0279d4a3f0d1686598bc5c088afd9cfa93de
Timmy Le <le.timmy94@gmail.com>
2023-06-06 18:11:42 -0400

    notes

--- UdemyApp1/UdemyApp1/ViewController.swift
+++ UdemyApp1/UdemyApp1/ViewController.swift
@@ -21,9 +21,10 @@ class ViewController: UIViewController {
         
     }
 
-
+    //sender UIButton is the action happening; i.e User touches screen. startImage contains the array, since theres 18 numbers we have to define the scope from 0-17
     @IBAction func switchLanguages(_ sender: UIButton) {
         startImage.image = [#imageLiteral(resourceName: "2"), #imageLiteral(resourceName: "10"), #imageLiteral(resourceName: "1"), #imageLiteral(resourceName: "12"), #imageLiteral(resourceName: "11"), #imageLiteral(resourceName: "13"), #imageLiteral(resourceName: "10"), #imageLiteral(resourceName: "4"), #imageLiteral(resourceName: "5"), #imageLiteral(resourceName: "6"), #imageLiteral(resourceName: "15"), #imageLiteral(resourceName: "14"), #imageLiteral(resourceName: "16"), #imageLiteral(resourceName: "8"), #imageLiteral(resourceName: "7"), #imageLiteral(resourceName: "17"), #imageLiteral(resourceName: "8"), #imageLiteral(resourceName: "18")][Int.random(in: 0...17)]
+        
         
         greetingImage += 1
     }
